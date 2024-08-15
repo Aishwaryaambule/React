409f892d7479f68de9ac71dbe2aba3e65dc475c4
Aishwarya Ambule <aishwaryaambule@gmail.com>
2024-08-15 21:23:50 +0530

    add a code

--- Projects/3-todo-app-version-two/src/components/TodoItems.jsx
+++ Projects/3-todo-app-version-two/src/components/TodoItems.jsx
@@ -1,11 +1,18 @@
+import styles from './TodoItems.module.css';
+
 import TodoItem from "./TodoItem";
 
 const TodoItems = ({ todoItems }) => {
   return (
-    <div className="items-container">
-    {todoItems.map(item =>   <TodoItem todoDate= {item.dueDate}todoName={item.name} />)}
-    
+    <div className={styles.itemsContainer}>
+      {todoItems.map((item) => (
+        <TodoItem
+          todoDate={item.dueDate}
+          todoName={item.name}
+        />
+      ))}
     </div>
   );
-}
+};
+
 export default TodoItems;
