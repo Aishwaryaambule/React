92a0b3b47e749ab4708716c2742203030e0bb958
Aishwarya Ambule <aishwaryaambule@gmail.com>
2024-08-23 21:38:03 +0530

    ee

--- 06 CurrencyConverter/src/App.jsx
+++ 06 CurrencyConverter/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl bg-orange-500'>Currency App with Chai</h1>
+    <h1 className='text-3xl bg-orange-500'>Currency App with Chai</h1>
     </>
   )
 }
