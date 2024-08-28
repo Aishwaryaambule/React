de4c8a0b0cf9f502bb9d890e504875e1af5c6c89
Aishwarya Ambule <aishwaryaambule@gmail.com>
2024-08-28 13:50:04 +0530

    dd

--- 06 CurrencyConverter/src/App.jsx
+++ 06 CurrencyConverter/src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
 
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState("0")
+
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo)
+  }
+
   return (
     <>
-    <h1 className='text-3xl bg-orange-500'>Currency App with Chai</h1>
+      <h1 className='text-3xl bg-orange-500'>Currency App with  Chai</h1>
     </>
   )
 }
